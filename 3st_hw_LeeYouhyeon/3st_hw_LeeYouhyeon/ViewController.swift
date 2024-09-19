c4d2c000f541ca68d44eae8f4a7785649539e9d8
LYH513 <peperony367@gmail.com>
2024-09-19 12:33:38 +0900

    과제3 프로젝트 생성

---
+++ 3st_hw_LeeYouhyeon/3st_hw_LeeYouhyeon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3st_hw_LeeYouhyeon
+//
+//  Created by 이유현 on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
