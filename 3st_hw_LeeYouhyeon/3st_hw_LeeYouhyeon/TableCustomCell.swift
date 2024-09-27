06664a1b38745dafc0708bde979ce0fabaca48e7
LYH513 <peperony367@gmail.com>
2024-09-27 15:04:13 +0900

    edit: 테이블뷰 추가 후 헤더 수정

---
+++ 3st_hw_LeeYouhyeon/3st_hw_LeeYouhyeon/TableCustomCell.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class TableCustomCell: UITableViewCell {
+    let image = UIImageView()
+    let label = UILabel()
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: "Cell")
+        contentView.addSubview(label)
+        contentView.addSubview(image)
+    }
+    
+    func setLabel(){
+        label.translatesAutoresizingMaskIntoConstraints = false
+        image.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            
+            image.topAnchor.constraint(equalTo: contentView.topAnchor),
+            image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+        ])
+    }
+}
