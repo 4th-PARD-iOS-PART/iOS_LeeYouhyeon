faf30a23fb902286958750189bd65f80d3c05b0c
LYH513 <peperony367@gmail.com>
2024-10-03 23:41:51 +0900

    add: comingSomm 페이지 헤더 추가

--- 4st_hw_LeeYouhyeon/4st_hw_LeeYouhyeon/Controllers/ComingViewController.swift
+++ 4st_hw_LeeYouhyeon/4st_hw_LeeYouhyeon/Controllers/ComingViewController.swift
@@ -26,16 +26,32 @@ class ComingViewController: UIViewController{
             return contents
         }()
         
+        let roundButton: UIButton = {
+            let button = UIButton()
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.backgroundColor = UIColor(red: 0.96, green: 0.04, blue: 0.08, alpha: 1)
+            button.layer.cornerRadius = 19/2  // 크기의 절반으로 설정해서 동그랗게
+            button.clipsToBounds = true
+            
+            if let buttonImage = UIImage(named: "smallbell") {
+                button.setImage(buttonImage, for: .normal)
+            }
+            return button
+        }()
+        
         let label: UILabel = {
             let labels = UILabel()
             labels.translatesAutoresizingMaskIntoConstraints = false
-            labels.text = "ComingSoon"
+            labels.text = "Notifications"
             labels.textColor = .white
+            labels.textAlignment = .center
+            labels.font = UIFont.boldSystemFont(ofSize: 16.91)
             return labels
         }()
         
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
+        contentView.addSubview(roundButton)
         contentView.addSubview(label)
         
         
@@ -55,9 +71,16 @@ class ComingViewController: UIViewController{
             contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
             
-            // contents들.
-            label.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 100),
-            label.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30),
+            // 벨 버튼
+            roundButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 13),
+            roundButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            roundButton.widthAnchor.constraint(equalToConstant: 19),  // 버튼 크기
+            roundButton.heightAnchor.constraint(equalToConstant: 19),
+            
+            // 라벨
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 13),
+            label.leadingAnchor.constraint(equalTo: roundButton.trailingAnchor, constant: 7),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -235),
         ])
         
     }
